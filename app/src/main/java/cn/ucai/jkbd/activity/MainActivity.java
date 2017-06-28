55e68f30141e04ea7178eabe97de6c5332868ac8
chunming1 <826030898@qq.com>
2017-06-29 00:10:02 +0800

    三个类作业

---
+++ app/src/main/java/cn/ucai/jkbd/activity/MainActivity.java
@@ -0,0 +1,40 @@
+package cn.ucai.jkbd.activity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;import android.view.View;
+import android.widget.Toast;
+
+import cn.ucai.jkbd.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent intent=new Intent();
+        setResult(RESULT_OK, intent);
+        finish();
+    }
+
+    public void test(android.view.View view) {
+        Toast.makeText(this,"测试",Toast.LENGTH_LONG).show();
+    }
+
+    public void test_all(android.view.View view) {
+
+    }
+
+    public void setting(android.view.View view) {
+
+    }
+
+    public void quit(android.view.View view) {
+
+    }
+}
